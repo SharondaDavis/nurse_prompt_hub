f27c35c564e942ee30ddcaadd817a135dc652fd1
Sharonda.Davis <165318629+SharondaDavis@users.noreply.github.com>
2025-06-28 21:58:27 -0400

    Fix Metro configuration getDevServer error

--- metro.config.js
+++ metro.config.js
@@ -8,4 +8,10 @@ config.resolver.platforms = ['ios', 'android', 'native', 'web'];
 // Add support for additional file extensions
 config.resolver.sourceExts.push('sql');
 
+// Fix getDevServer to be a function that returns the original object
+if (config.getDevServer && typeof config.getDevServer !== 'function') {
+  const originalDevServer = config.getDevServer;
+  config.getDevServer = () => originalDevServer;
+}
+
 module.exports = config;
