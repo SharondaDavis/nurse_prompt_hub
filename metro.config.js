f6adb6b2a7e70e905e77781abf1f0cafba1ca01a
Sharonda.Davis <165318629+SharondaDavis@users.noreply.github.com>
2025-06-28 22:28:36 -0400

    Fix Expo Router and Metro Setup

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,2 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Ensure all platforms are supported
-config.resolver.platforms = ['ios', 'android', 'native', 'web'];
-
-// Add support for additional file extensions
-config.resolver.sourceExts.push('sql');
-
-module.exports = config;
+const { getDefaultConfig } = require('@expo/metro-config');
+module.exports = getDefaultConfig(__dirname);
