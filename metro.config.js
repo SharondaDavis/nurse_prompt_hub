575947dbf36392605529c441a8c1658b9b3f8357
Sharonda.Davis <165318629+SharondaDavis@users.noreply.github.com>
2025-06-29 13:08:49 -0400

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,4 @@
-const { getDefaultConfig } = require('expo/metro-config');
+// metro.config.js
+const { getDefaultConfig } = require('@expo/metro-config');
+module.exports = getDefaultConfig(__dirname);
 
-const config = getDefaultConfig(__dirname);
-
-// Add “expo-router” to the list of modules that Metro will transform:
-config.resolver.blacklistRE = null;
-config.transformer = {
-  ...config.transformer,
-  // ensure .cjs files (used by expo-router) get run through Babel
-  babelTransformerPath: require.resolve('metro-react-native-babel-transformer'),
-};
-config.resolver.sourceExts = [...config.resolver.sourceExts, 'cjs'];
-
-module.exports = config;
